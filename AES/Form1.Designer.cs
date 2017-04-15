31e542e0d6a02e34bd110a6b5da7c523d3c0e5dd
djlaudio <luis.m24a@gmail.com>
2017-04-15 16:52:04 -0600

    Version inicial pasada de JAVA

---
+++ AES/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCifrar = new System.Windows.Forms.Button();
+            this.btnDescifrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCifrar
+            // 
+            this.btnCifrar.Location = new System.Drawing.Point(572, 328);
+            this.btnCifrar.Name = "btnCifrar";
+            this.btnCifrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCifrar.TabIndex = 0;
+            this.btnCifrar.Text = "Cifrar";
+            this.btnCifrar.UseVisualStyleBackColor = true;
+            this.btnCifrar.Click += new System.EventHandler(this.btnCifrar_Click);
+            // 
+            // btnDescifrar
+            // 
+            this.btnDescifrar.Location = new System.Drawing.Point(491, 328);
+            this.btnDescifrar.Name = "btnDescifrar";
+            this.btnDescifrar.Size = new System.Drawing.Size(75, 23);
+            this.btnDescifrar.TabIndex = 1;
+            this.btnDescifrar.Text = "Descifrar";
+            this.btnDescifrar.UseVisualStyleBackColor = true;
+            this.btnDescifrar.Click += new System.EventHandler(this.btnDescifrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 363);
+            this.Controls.Add(this.btnDescifrar);
+            this.Controls.Add(this.btnCifrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCifrar;
+        private System.Windows.Forms.Button btnDescifrar;
+    }
+}
+
---
+++ AES/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCifrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDescifrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
