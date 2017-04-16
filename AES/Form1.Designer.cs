4cd1783f0f584c631db7b652f5138256e092320e
drodriguez1988 <rodriguezvalverde@gmail.com>
2017-04-16 15:02:04 -0600

    Cifrado_Bien
    Este punto logra cifrar

--- AES/Form1.Designer.cs
+++ AES/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.btnCifrar = new System.Windows.Forms.Button();
             this.btnDescifrar = new System.Windows.Forms.Button();
+            this.rtGenerico = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // btnCifrar
@@ -52,11 +53,20 @@
             this.btnDescifrar.UseVisualStyleBackColor = true;
             this.btnDescifrar.Click += new System.EventHandler(this.btnDescifrar_Click);
             // 
+            // rtGenerico
+            // 
+            this.rtGenerico.Location = new System.Drawing.Point(12, 206);
+            this.rtGenerico.Name = "rtGenerico";
+            this.rtGenerico.Size = new System.Drawing.Size(451, 110);
+            this.rtGenerico.TabIndex = 2;
+            this.rtGenerico.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(659, 363);
+            this.Controls.Add(this.rtGenerico);
             this.Controls.Add(this.btnDescifrar);
             this.Controls.Add(this.btnCifrar);
             this.Name = "Form1";
@@ -69,6 +79,7 @@
 
         private System.Windows.Forms.Button btnCifrar;
         private System.Windows.Forms.Button btnDescifrar;
+        private System.Windows.Forms.RichTextBox rtGenerico;
     }
 }
 
