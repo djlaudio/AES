88160d76c66622165c0e371f20207762e19609d6
drodriguez1988 <rodriguezvalverde@gmail.com>
2017-04-19 14:05:02 -0600

    Cambios en interfaz
    Cambios en interfaz

--- AES/Form1.Designer.cs
+++ AES/Form1.Designer.cs
@@ -30,7 +30,12 @@
         {
             this.btnCifrar = new System.Windows.Forms.Button();
             this.btnDescifrar = new System.Windows.Forms.Button();
-            this.rtGenerico = new System.Windows.Forms.RichTextBox();
+            this.rtDown = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbKey = new System.Windows.Forms.TextBox();
+            this.rtUp = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnCifrar
@@ -53,25 +58,75 @@
             this.btnDescifrar.UseVisualStyleBackColor = true;
             this.btnDescifrar.Click += new System.EventHandler(this.btnDescifrar_Click);
             // 
-            // rtGenerico
+            // rtDown
             // 
-            this.rtGenerico.Location = new System.Drawing.Point(12, 206);
-            this.rtGenerico.Name = "rtGenerico";
-            this.rtGenerico.Size = new System.Drawing.Size(451, 110);
-            this.rtGenerico.TabIndex = 2;
-            this.rtGenerico.Text = "";
+            this.rtDown.Location = new System.Drawing.Point(84, 222);
+            this.rtDown.Name = "rtDown";
+            this.rtDown.Size = new System.Drawing.Size(382, 110);
+            this.rtDown.TabIndex = 2;
+            this.rtDown.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Clave:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Texto plano:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tbKey
+            // 
+            this.tbKey.Location = new System.Drawing.Point(84, 47);
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(168, 20);
+            this.tbKey.TabIndex = 5;
+            this.tbKey.Text = "1234567890ABCDEF";
+            // 
+            // rtUp
+            // 
+            this.rtUp.Location = new System.Drawing.Point(84, 120);
+            this.rtUp.Name = "rtUp";
+            this.rtUp.Size = new System.Drawing.Size(382, 96);
+            this.rtUp.TabIndex = 6;
+            this.rtUp.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Texto cifrado:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(659, 363);
-            this.Controls.Add(this.rtGenerico);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rtUp);
+            this.Controls.Add(this.tbKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtDown);
             this.Controls.Add(this.btnDescifrar);
             this.Controls.Add(this.btnCifrar);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "AES";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -79,7 +134,12 @@
 
         private System.Windows.Forms.Button btnCifrar;
         private System.Windows.Forms.Button btnDescifrar;
-        private System.Windows.Forms.RichTextBox rtGenerico;
+        private System.Windows.Forms.RichTextBox rtDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbKey;
+        private System.Windows.Forms.RichTextBox rtUp;
+        private System.Windows.Forms.Label label3;
     }
 }
 
